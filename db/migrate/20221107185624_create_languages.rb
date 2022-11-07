597d3ae133d68ba6ed0ae9454d8f8c06617179b4
Muthiira <calebmuchiri48@gmail.com>
2022-11-07 22:04:18 +0300

    language_table

--- db/migrate/20221107185624_create_languages.rb
+++ db/migrate/20221107185624_create_languages.rb
@@ -1,4 +1,10 @@
 class CreateLanguages < ActiveRecord::Migration[6.1]
   def change
+    create_table :languages do |t|
+      t.string :language
+      t.integer :experience
+      t.integer :dev_id
+      t.integer :project_id
+    end
   end
 end
