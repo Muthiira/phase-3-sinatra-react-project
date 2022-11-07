a6310a550f1dd57029c1cd90b1778a47d6f951e9
Muthiira <calebmuchiri48@gmail.com>
2022-11-07 22:07:04 +0300

    devs_table

--- db/migrate/20221107185649_create_devs.rb
+++ db/migrate/20221107185649_create_devs.rb
@@ -1,4 +1,10 @@
 class CreateDevs < ActiveRecord::Migration[6.1]
   def change
+    create_table :devs do |t|
+      t.string :name
+      t.string :image_url
+      t.string :github_url
+      t.integer :age
+    end
   end
 end
