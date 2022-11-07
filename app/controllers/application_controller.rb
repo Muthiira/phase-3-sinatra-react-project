6ddaeb256420bce581f8bced70e6c99b789cbb31
Muthiira <calebmuchiri48@gmail.com>
2022-11-08 00:40:31 +0300

    get all devs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,37 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get '/devs' do
+    Dev.all.to_json
+  end
+
+#   get '/dev/:id' do
+#     Dev.find(params[:id]).to_json
+#   end
+
+#   get '/dev/:id/languages' do
+#     Dev.find(params[:id]).to_json(include: :languages)
+#   end
+
+#   post '/language' do
+#     language = Language.create(
+#         language: params[:language],
+#         experience: params[:experience],
+#         dev_id: params[:dev_id],
+#         project_id: params[:project_id]
+#     )
+#     language.to_json
+# end
+
+
+# get '/languages' do
+#   Language.all.to_json(include: :dev)
+# end
+
+
+# get '/language/:id' do
+#   Language.find(params[:id]).to_json(include: :dev)
+# end
+
+
 end
